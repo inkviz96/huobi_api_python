0c0beca6ac167eefe184f4dd83daaff10eb9e14c
Semyon <b-semen-b@mail.ru>
2022-04-08 00:33:35 +0300

    hotfix

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as readme_file:
 
 setup(
     name='HuobiPoolApi',
-    version='0.0.4',
+    version='0.0.5',
     license='MIT',
     author="Inkviz96",
     author_email='b-semen-b@mail.ru',
