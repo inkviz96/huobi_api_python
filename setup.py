1c33772c2d3a56dfb70c1faec3089ea7c2654c93
Semyon <b-semen-b@mail.ru>
2022-04-07 15:06:47 +0300

    new version

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("Readme.md", "r") as readme_file:
 
 setup(
     name='Huobi Pool api',
-    version='0.0.1',
+    version='0.1.0',
     license='MIT',
     author="Inkviz96",
     author_email='b-semen-b@mail.ru',
