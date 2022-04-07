423b1d45fef7d5bc0cf21d09b8c09198a5037a19
Semyon <b-semen-b@mail.ru>
2022-04-07 18:56:49 +0300

    hotfix

--- huobi_api/client.py
+++ huobi_api/client.py
@@ -5,27 +5,27 @@ class Client:
         self.access_api_key = access_api_key
         self.secret_key = secret_key
 
-        from api.wallet import get_deposit_address
-        from api.wallet import get_withdraw_quota
-        from api.wallet import get_withdraw_address
-        from api.wallet import withdraw
-        from api.wallet import withdrawal_order_by_order_id
-        from api.wallet import cancel_withdraw
-        from api.wallet import get_deposit_withdraw
+        from huobi_api.api.wallet import get_deposit_address
+        from huobi_api.api.wallet import get_withdraw_quota
+        from huobi_api.api.wallet import get_withdraw_address
+        from huobi_api.api.wallet import withdraw
+        from huobi_api.api.wallet import withdrawal_order_by_order_id
+        from huobi_api.api.wallet import cancel_withdraw
+        from huobi_api.api.wallet import get_deposit_withdraw
 
-        from api.account import get_accounts
-        from api.account import get_account_balance
-        from api.account import get_total_valuation
-        from api.account import get_asset_valuation
-        from api.account import account_transfer
-        from api.account import get_account_history
-        from api.account import get_account_ledger
-        from api.account import transfer_fund_between_spot_account
-        from api.account import get_point_balance
-        from api.account import point_transfer
+        from huobi_api.api.account import get_accounts
+        from huobi_api.api.account import get_account_balance
+        from huobi_api.api.account import get_total_valuation
+        from huobi_api.api.account import get_asset_valuation
+        from huobi_api.api.account import account_transfer
+        from huobi_api.api.account import get_account_history
+        from huobi_api.api.account import get_account_ledger
+        from huobi_api.api.account import transfer_fund_between_spot_account
+        from huobi_api.api.account import get_point_balance
+        from huobi_api.api.account import point_transfer
 
-        from api.reference import get_currencys_settings
-        from api.reference import supported_currencys
-        from api.reference import chains_information
+        from huobi_api.api.reference import get_currencys_settings
+        from huobi_api.api.reference import supported_currencys
+        from huobi_api.api.reference import chains_information
 
-        from signing import sign_request
+        from huobi_api.signing import sign_request
