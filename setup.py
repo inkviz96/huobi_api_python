d4972b402af46dda191dd000a68d41717c0b5c45
Semyon <b-semen-b@mail.ru>
2022-04-07 18:17:10 +0300

    hotfix

--- setup.py
+++ setup.py
@@ -5,20 +5,17 @@ with open("Readme.md", "r") as readme_file:
 
 setup(
     name='Huobi Pool api',
-    version='0.1.1',
+    version='0.1.2',
     license='MIT',
     author="Inkviz96",
     author_email='b-semen-b@mail.ru',
     packages=find_packages(),
-    python_requires='>=3.8',
     url='https://github.com/inkviz96/huobi_api_python',
     keywords='huobi api pool',
     description="Huobi Pool api lib for easy work with huobi",
     long_description=readme,
     long_description_content_type="text/markdown",
-    install_requires=[
-          'requests>=2.27.1',
-      ],
+    install_requires=["requests>=2.27.1"],
     classifiers=[
         "Programming Language :: Python :: 3.8",
         "License :: OSI Approved :: GNU General Public License v3 (GPLv3)",
